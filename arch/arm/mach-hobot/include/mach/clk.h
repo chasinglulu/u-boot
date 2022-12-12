b74576535c553380d3ec529e088767aef90ede79
Charleye <wangkart@aliyun.com>
2022-12-12 20:45:19 +0800

    hobot: Enable Cadence eth driver

---
+++ arch/arm/mach-hobot/include/mach/clk.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2022 Hobot Co.Ltd.
+ *
+ */
+
+#ifndef __ASM_ARM_ARCH_CLK_H__
+#define __ASM_ARM_ARCH_CLK_H__
+
+static inline unsigned long get_macb_pclk_rate(unsigned int dev_id)
+{
+    return 20000000UL;
+}
+
+#endif
