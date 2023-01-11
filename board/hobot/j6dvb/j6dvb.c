b6f994fad75bce3d90a615ab7a55b7124e772701
Charleye <wangkart@aliyun.com>
2023-01-11 13:31:32 +0800

    arm; sigi: Setup usable ram top

--- board/hobot/j6dvb/j6dvb.c
+++ board/hobot/j6dvb/j6dvb.c
@@ -7,6 +7,10 @@
 #include <dm.h>
 #include <init.h>
 #include <asm/system.h>
+#include <linux/sizes.h>
+
+extern uint32_t ram_base_msb;
+DECLARE_GLOBAL_DATA_PTR;
 
 #ifndef CONFIG_SPL_BUILD
 int board_early_init_f(void)
@@ -20,4 +24,12 @@ int board_init(void)
 	printf("EL Level:\tEL%d\n", current_el());
 
 	return 0;
+}
+
+ulong board_get_usable_ram_top(ulong total_size)
+{
+	if (ram_base_msb >= 32)
+		return CONFIG_SYS_DDR_BASE + gd->ram_size;
+	else
+		return CONFIG_SYS_OCM_BASE + SZ_32M;
 }
