099250d2fb3952631b710ce025c1f29e3c644720
Charleye <wangkart@aliyun.com>
2023-05-30 13:50:19 +0800

    arm: sigi-virt: Update partitions in nor nodes

--- board/hobot/virt-sigi/virt-sigi.c
+++ board/hobot/virt-sigi/virt-sigi.c
@@ -7,6 +7,9 @@
 #include <dm.h>
 #include <init.h>
 #include <asm/system.h>
+#include "jffs2/load_kernel.h"
+#include "fdt_support.h"
+#include "mtd_node.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -27,6 +30,16 @@ int board_init(void)
 #if defined (CONFIG_OF_LIBFDT) && defined (CONFIG_OF_BOARD_SETUP)
 int ft_board_setup(void *blob, struct bd_info *bd)
 {
+#if defined(CONFIG_FDT_FIXUP_PARTITIONS)
+	static struct node_info nodes[] = {
+		{ "cfi-flash", MTD_DEV_TYPE_NOR, },
+	};
+
+	/* Update partition nodes using info from mtdparts env var */
+	puts("   Updating MTD partitions...\n");
+	fdt_fixup_mtdparts(blob, nodes, ARRAY_SIZE(nodes));
+#endif
+
 	return 0;
 }
 #endif
