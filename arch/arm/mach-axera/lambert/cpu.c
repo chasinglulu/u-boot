13e0ff427f26b0d24bdcd03bc942c52b4904c65c
Charleye <wangkart@aliyun.com>
2023-10-11 14:08:17 +0800

    arm: Add new Lambert SoC support

---
+++ arch/arm/mach-axera/lambert/cpu.c
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0+
+ *
+ * Copyright (C) 2023 Charleye <wangkart@aliyun.com>
+ */
+
+#include <common.h>
+
+static u32 reset_cause = -1;
+
+u32 lmt_get_reset_cause(void)
+{
+	return reset_cause;
+}
+
+#if defined(CONFIG_DISPLAY_CPUINFO) && !defined(CONFIG_SPL_BUILD)
+static char *get_reset_cause(void)
+{
+	switch (lmt_get_reset_cause()) {
+	case 0x0001:
+	case 0x0011:
+		return "POR";
+	case 0x0004:
+		return "CSU";
+	case 0x0010:
+		return "WDOG";
+	case 0x0040:
+		return "JTAG SW";
+	default:
+		return "unknown reset";
+	}
+}
+
+int print_cpuinfo(void)
+{
+	printf("Reset cause: %s\n", get_reset_cause());
+
+	return 0;
+}
+#endif
---
+++ arch/arm/mach-axera/lambert/lambert.c
@@ -0,0 +1,57 @@
+/* SPDX-License-Identifier: GPL-2.0+
+ *
+ * Copyright (C) 2023 Charleye <wangkart@aliyun.com>
+ */
+
+#include <common.h>
+#include <asm/armv8/mmu.h>
+#include <linux/sizes.h>
+#include <asm/sections.h>
+#include <fdt_support.h>
+#include <init.h>
+#include <cpu_func.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+typedef enum mmap_region_type {
+	MMAP_DEVICE_MMIO,
+	MMAP_HIGH_DDR,
+	MMAP_END,
+} mmap_region_t;
+
+#define MMAP_DEVICE_BASE	0x00020000UL
+#define MMAP_HIGH_DDR_BASE	0x400000000UL
+
+static struct mm_region lmt_mem_map[] = {
+	[MMAP_DEVICE_MMIO] = {
+		.virt = MMAP_DEVICE_BASE,
+		.phys = MMAP_DEVICE_BASE,
+		.size = SZ_1G,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			PTE_BLOCK_NON_SHARE | PTE_BLOCK_PXN |
+			PTE_BLOCK_UXN
+	},
+	[MMAP_HIGH_DDR] = {
+		.virt = MMAP_HIGH_DDR_BASE,
+		.phys = MMAP_HIGH_DDR_BASE,
+		.size = SZ_4G,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			PTE_BLOCK_INNER_SHARE
+	}, {
+		/* List terminator */
+		0,
+	}
+};
+
+struct mm_region *mem_map = lmt_mem_map;
+
+void reset_cpu(void)
+{
+	return;
+}
+
+void enable_caches(void)
+{
+	icache_enable();
+	dcache_enable();
+}
