b9f6cef0fc1fa3171d33a767d0b2810e7a133777
chasinglulu <wangkartx@gmail.com>
2023-11-15 20:43:49 +0800

    arm: hobot: Enable semihosting support

--- include/configs/hobot-j6.h
+++ include/configs/hobot-j6.h
@@ -6,16 +6,6 @@
 #ifndef __HOBOT_J6SOC_H
 #define __HOBOT_J6SOC_H
 
-#define HOBOT_SIP_FUNC_ID	0x82000009
-
-/* ---------------------------------------------------------------------
- * Board boot configuration
- */
-/* Environment options */
-
-#define FASTBOOT_USB_CMD "fastboot_usb_cmd=fastboot usb 0\0"
-#define FASTBOOT_NET_CMD "fastboot_net_cmd=fastboot net\0"
-
 #if CONFIG_IS_ENABLED(CMD_MMC)
 # define BOOT_TARGET_MMC(func) func(MMC, mmc, 0)
 # define BOOT_TARGET_MMC1(func) func(MMC, mmc, 1)
@@ -54,30 +44,70 @@
 # define BOOT_TARGET_DHCP(func)
 #endif
 
-#define BOOT_TARGET_DEVICES(func) \
-       BOOT_TARGET_MMC(func) \
-       BOOT_TARGET_MMC1(func) \
-       BOOT_TARGET_SCSI(func) \
-       BOOT_TARGET_NVME(func) \
-       BOOT_TARGET_VIRTIO(func) \
-       BOOT_TARGET_DHCP(func) \
-       BOOT_TARGET_USB(func)
+/*
+ * Boot by loading an Android image, or kernel, initrd and FDT through
+ * semihosting into DRAM.
+ */
+#define BOOTENV_DEV_SMH(devtypeu, devtypel, instance) \
+	"bootcmd_smh= " 						\
+		"if smhload ${boot_name} ${boot_addr_r}; then"		\
+		"  setenv bootargs;"					\
+		"  abootimg addr ${boot_addr_r};"			\
+		"  abootimg get dtb --index=0 fdt_addr_r;"		\
+		"  bootm ${boot_addr_r} ${boot_addr_r} ${fdt_addr_r};"	\
+		"else"							\
+		"  if smhload ${kernel_name} ${kernel_addr_r}; then"	\
+		"    setenv fdt_high 0xffffffffffffffff;"		\
+		"    setenv initrd_high 0xffffffffffffffff;"		\
+		"    smhload ${fdtfile} ${fdt_addr_r};"			\
+		"    smhload ${ramdisk_name} ${ramdisk_addr_r} ramdisk_end;" \
+		"    fdt addr ${fdt_addr_r};"				\
+		"    fdt resize;"					\
+		"    fdt chosen ${ramdisk_addr_r} ${ramdisk_end};"	\
+		"    booti $kernel_addr_r - $fdt_addr_r;"		\
+		"  fi;"							\
+		"fi\0"
+#define BOOTENV_DEV_NAME_SMH(devtypeu, devtypel, instance) "smh "
+
+#if CONFIG_IS_ENABLED(SEMIHOSTING)
+# define BOOT_TARGET_SMH(func) func(SMH, smh, na)
+#else
+# define BOOT_TARGET_SMH(func)
+#endif
+
+#define EXTRA_ENV_NAMES                                                    \
+		"kernel_name=Image\0"                                              \
+		"ramdisk_name=rootfs.cpio.lz4\0"                                   \
+		"fdtfile=hobot-sigi-virt.dtb\0"                                    \
+		"boot_name=boot.img\0"
+
+#define BOOT_TARGET_DEVICES(func)        \
+		BOOT_TARGET_SMH(func)            \
+		BOOT_TARGET_MMC(func)            \
+		BOOT_TARGET_MMC1(func)           \
+		BOOT_TARGET_SCSI(func)           \
+		BOOT_TARGET_NVME(func)           \
+		BOOT_TARGET_VIRTIO(func)         \
+		BOOT_TARGET_DHCP(func)           \
+		BOOT_TARGET_USB(func)
 
 #include <config_distro_bootcmd.h>
 
-#define CONFIG_EXTRA_ENV_SETTINGS		\
-		"stdout=serial\0" 				\
-		"stdin=serial\0" 				\
-		"stderr=serial\0"				\
-		"scriptaddr=0x3081000000\0"     \
-		"fdt_addr_r=0x3081800000\0"		\
-		"xen_addr_r=0x3082000000\0"		\
-		"kernel_addr_r=0x3085000000\0"  \
-		"domu1_addr_r=0x3087000000\0"	\
-		"ramdisk_addr_r=0x3089000000\0" \
-		"domu1_ramdisk_addr_r=0x3090000000\0"	\
-		"domu2_addr_r=0x3092000000\0"	\
-		"domu2_ramdisk_addr_r=0x3094000000\0"	\
+#define CONFIG_EXTRA_ENV_SETTINGS                                  \
+		"stdout=serial\0"                                          \
+		"stdin=serial\0"                                           \
+		"stderr=serial\0"                                          \
+		"scriptaddr=0x3081000000\0"                                \
+		"fdt_addr_r=0x3081800000\0"                                \
+		"xen_addr_r=0x3082000000\0"                                \
+		"kernel_addr_r=0x3085000000\0"                             \
+		"domu1_addr_r=0x3087000000\0"                              \
+		"ramdisk_addr_r=0x3089000000\0"                            \
+		"domu1_ramdisk_addr_r=0x3090000000\0"                      \
+		"domu2_addr_r=0x3092000000\0"                              \
+		"domu2_ramdisk_addr_r=0x3094000000\0"                      \
+		"boot_addr_r=0x3081000000\0"                               \
+		EXTRA_ENV_NAMES                                            \
 		BOOTENV
 
 #endif
