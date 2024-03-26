cbd3d1cd160b17f47397bfb999eae87a42084b07
chasinglulu <wangkartx@gmail.com>
2024-03-26 19:58:19 +0800

    arm: Adding new laguna safety island support

---
+++ board/axera/qemu-virt/laguna_r5f/board.c
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2024 Charleye <wangkart@aliyun.com>
+ */
+
+#include <common.h>
+#include <fdtdec.h>
+#include <init.h>
+
+int board_init(void)
+{
+	return 0;
+}
+
+int dram_init_banksize(void)
+{
+	return fdtdec_setup_memory_banksize();
+}
+
+int dram_init(void)
+{
+	if (fdtdec_setup_mem_size_base() != 0)
+		return -EINVAL;
+
+	return 0;
+}
