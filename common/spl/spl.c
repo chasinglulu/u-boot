ab9356ea2783f2c204dc0fcc0de6452c883c0630
Lukas Funke <lukas.funke@weidmueller.com>
2024-03-27 13:11:52 +0100

    spl: Introduce SoC specific init function
    Some architectures use spl_board_init() in their SoC specific
    implementation. Board developers should be able to add board specific
    implementation via spl_board_init(). Hence, introduce a spl_soc_init()
    method which is called right before spl_board_init() for SoC
    specific implementation.
    
    Signed-off-by: Lukas Funke <lukas.funke@weidmueller.com>
    Reviewed-by: Devarsh Thakkar <devarsht@ti.com>

--- common/spl/spl.c
+++ common/spl/spl.c
@@ -773,6 +773,9 @@ void board_init_r(gd_t *dummy1, ulong dummy2)
 	if (CONFIG_IS_ENABLED(GPIO_HOG))
 		gpio_hog_probe_all();
 
+	if (CONFIG_IS_ENABLED(SOC_INIT))
+		spl_soc_init();
+
 #if CONFIG_IS_ENABLED(BOARD_INIT)
 	spl_board_init();
 #endif
