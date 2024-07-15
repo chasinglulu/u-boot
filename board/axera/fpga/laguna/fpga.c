bbe8dc94d0cdb090914b3d403ff7e3c1dc30f61f
chasinglulu <wangkartx@gmail.com>
2024-07-15 16:58:18 +0800

    arm: laguna: rewrite uart clock gate and reset initialization

--- board/axera/fpga/laguna/fpga.c
+++ board/axera/fpga/laguna/fpga.c
@@ -15,20 +15,65 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#define PHEIPH_SYS_CK_RST_BASE  0x0E001000
-void uart_clk_enable(void)
+#if defined(CONFIG_LUA_PERIPH_CRU)
+#define PERIPH_SWRST0_RSTVAL            0x7FFFFFFF
+#define PHEIPH_CLK_GATE_ENABLE_RSTVAL   0xE0000000
+void main_uart_clk_enable(void)
 {
-	writel(0x0, PHEIPH_SYS_CK_RST_BASE + 0x40);
-	writel(0xFFFFFFFF, PHEIPH_SYS_CK_RST_BASE + 0x120);
-	udelay(10);
-	writel(0x7FE00, PHEIPH_SYS_CK_RST_BASE + 0x40);
-	writel(0xFFE007FF, PHEIPH_SYS_CK_RST_BASE + 0x120);
+	unsigned int reg;
+
+	writel(PHEIPH_CLK_GATE_ENABLE_RSTVAL,
+		   (CONFIG_LUA_PERIPH_CRU_BASE +
+		     CONFIG_LUA_PERIPH_CLK_GATE_REG0));
+	udelay(1);
+
+	/* Enable UART1 Clock Gate */
+	reg = readl(CONFIG_LUA_PERIPH_CRU_BASE +
+			    CONFIG_LUA_PERIPH_CLK_GATE_REG0);
+	reg |= BIT(9);    /* UART1 PCLK */
+	reg |= BIT(14);   /* UART1 CLK */
+
+	reg |= BIT(10);   /* UART2 PCLK */
+	reg |= BIT(15);   /* UART2 CLK */
+
+	reg |= BIT(11);   /* UART3 PCLK */
+	reg |= BIT(16);   /* UART3 CLK */
+
+	reg |= BIT(12);   /* UART4 PCLK */
+	reg |= BIT(17);   /* UART4 CLK */
+
+	reg |= BIT(13);   /* UART5 PCLK */
+	reg |= BIT(18);   /* UART5 CLK */
+	writel(reg, (CONFIG_LUA_PERIPH_CRU_BASE +
+		     CONFIG_LUA_PERIPH_CLK_GATE_REG0));
 }
+
+void deassert_uart1_reset(void)
+{
+	unsigned int reg;
+
+	writel(PERIPH_SWRST0_RSTVAL,
+		    (CONFIG_LUA_PERIPH_CRU_BASE +
+		      CONFIG_LUA_PERIPH_SWRST_REG0));
+	udelay(5);
+	/* Deassert UART1 */
+	reg = readl(CONFIG_LUA_PERIPH_CRU_BASE +
+			    CONFIG_LUA_PERIPH_SWRST_REG0);
+	reg &= ~BIT(11);    /* UART1 SW PRST */
+	reg &= ~BIT(16);    /* UART1 SW RST */
+	writel(reg, (CONFIG_LUA_PERIPH_CRU_BASE +
+		     CONFIG_LUA_PERIPH_SWRST_REG0));
+}
+#else
+void main_uart_clk_enable(void) {};
+void deassert_uart1_reset(void) {};
+#endif
 
 #ifdef CONFIG_BOARD_EARLY_INIT_F
 int board_early_init_f(void)
 {
-	uart_clk_enable();
+	main_uart_clk_enable();
+	deassert_uart1_reset();
 	return 0;
 }
 #endif
@@ -36,7 +81,8 @@ int board_early_init_f(void)
 #ifdef CONFIG_DEBUG_UART_BOARD_INIT
 void board_debug_uart_init(void)
 {
-	uart_clk_enable();
+	main_uart_clk_enable();
+	deassert_uart1_reset();
 }
 #endif
 
