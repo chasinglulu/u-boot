15972b70f34d9d4eff91d44d519694e629e57608
chasinglulu <wangkartx@gmail.com>
2024-07-15 17:01:07 +0800

    reset: Add laguna syscon reset node within dts

---
+++ include/dt-bindings/mfd/laguna-sysctl.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2024 Charleye <wangkart@aliyun.com>
+ */
+
+
+#ifndef LAGUNA_SYSCTL_H
+#define LAGUNA_SYSCTL_H
+
+#define LAGUNA_SYSCTL_COMM_SWRST         0x120
+#define LAGUNA_SYSCTL_CPU_SWRST          0x120
+#define LAGUNA_SYSCTL_FLASH_SWRST        0x120
+#define LAGUNA_SYSCTL_PERIPH_SWRST0      0x120
+#define LAGUNA_SYSCTL_PERIPH_SWRST1      0x124
+#define LAGUNA_SYSCTL_PERIPH_CAN_SWRST   0x164
+
+#endif
