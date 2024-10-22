0ddd85c30520d88318e6324547d76427701bd7e8
chasinglulu <wangkartx@gmail.com>
2024-10-22 13:41:37 +0800

    arm: laguna: Add clocks with clear name

---
+++ include/dt-bindings/mfd/laguna-cru.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2024 Charleye <wangkart@aliyun.com>
+ */
+
+#ifndef LAGUNA_CRU_H
+#define LAGUNA_CRU_H
+
+/* cpu clock & rst */
+#define LAGUNA_CPU_CLK_MUX_0             0x00
+#define LAGUNA_CPU_CLK_G_EB_0            0x40
+#define LAGUNA_CPU_SW_RST_0              0x120
+
+/* common clock & rst */
+#define LAGUNA_COMM_CLK_MUX_0            0x00
+#define LAGUNA_COMM_CLK_M_EB_0           0x20
+#define LAGUNA_COMM_CLK_G_EB_0           0x40
+#define LAGUNA_COMM_SW_RST_0             0x120
+
+/* flash clock & rst */
+#define LAGUNA_FLASH_CLK_MUX_0            0x00
+#define LAGUNA_FLASH_CLK_M_EB_0           0x20
+#define LAGUNA_FLASH_CLK_G_EB_0           0x40
+#define LAGUNA_FLASH_SW_RST_0             0x120
+
+/* periph clock & rst */
+#define LAGUNA_PERIPH_CLK_MUX_0          0x00
+#define LAGUNA_PERIPH_CLK_M_EB_0         0x20
+#define LAGUNA_PERIPH_CLK_G_EB_0         0x40
+#define LAGUNA_PERIPH_CLK_G_EB_1         0x44
+#define LAGUNA_PERIPH_CLK_M_DIV_0        0x80
+#define LAGUNA_PERIPH_SW_RST_0           0x120
+#define LAGUNA_PERIPH_SW_RST_1           0x124
+#define LAGUNA_PERIPH_CAN_SOFT_RESET     0x164
+
+#endif
--- include/dt-bindings/mfd/laguna-syscon.h
+++ include/dt-bindings/mfd/laguna-syscon.h
@@ -3,24 +3,15 @@
  * Copyright (C) 2024 Charleye <wangkart@aliyun.com>
  */
 
+#ifndef LAGUNA_SYSCON_H
+#define LAGUNA_SYSCON_H
 
-#ifndef LAGUNA_SYSCTL_H
-#define LAGUNA_SYSCTL_H
-
-/* ck_rst regions */
-#define LAGUNA_SYSCTL_COMM_SWRST         0x120
-#define LAGUNA_SYSCTL_CPU_SWRST          0x120
-#define LAGUNA_SYSCTL_FLASH_SWRST        0x120
-#define LAGUNA_SYSCTL_PERIPH_SWRST0      0x120
-#define LAGUNA_SYSCTL_PERIPH_SWRST1      0x124
-#define LAGUNA_SYSCTL_PERIPH_CAN_SWRST   0x164
-
-/* glb regions */
+/* glb register */
 #define LAGUNA_SYSCON_PERIPH_EMAC       0x28
 #define LAGUNA_SYSCON_PERIPH_UART       0x30
 #define LAGUNA_SYSCON_PERIPH_EMAC1      0x6C
 
-/* Periph UART register field */
+/* periph glb UART register field */
 #define UART_XFER_RS232_2LINE         0
 #define UART_XFER_RS232_4LINE         1
 #define UART_XFER_IRDA_2LINE          2
