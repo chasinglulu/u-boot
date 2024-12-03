a0b858cddd5d533837f6cce26366916b1e561ced
chasinglulu <wangkart@aliyun.com>
2024-12-03 19:21:04 +0800

    cmd: Add block command

---
+++ cmd/block.c
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2024, Charleye <wangkart@aliyun.com>
+ *
+ */
+
+#include <common.h>
+#include <blk.h>
+#include <command.h>
+#include <linux/err.h>
+#include <dm.h>
+#include <mapmem.h>
+#if defined(CONFIG_DM_MTD)
+#include <mtd.h>
+#endif
+
+static int blk_dev = -1;
+static int if_devnum = -1;
+static enum if_type if_type = IF_TYPE_UNKNOWN;
+
+static int do_mtdblock(struct cmd_tbl *cmdtp, int flag, int argc,
+                        char *const argv[])
+{
+#if defined(CONFIG_DM_MTD)
+	mtd_probe_devices();
+#endif
+
+	return blk_common_cmd(argc, argv, IF_TYPE_MTD, &if_devnum);
+}
+
+static int do_mmc_block(struct cmd_tbl *cmdtp, int flag, int argc,
+                        char *const argv[])
+{
+	return blk_common_cmd(argc, argv, IF_TYPE_MMC, &if_devnum);
+}
+
+static void block_display_line(struct udevice *dev, int index)
+{
+	printf("%-3i %c %s", index,
+	       dev_get_flags(dev) & DM_FLAG_ACTIVATED ? '*' : ' ',
+	       dev->name);
+	if (dev->seq_ != -1)
+		printf(", seq %d", dev_seq(dev));
+	puts("\n");
+}
+
+static int do_block(struct cmd_tbl *cmdtp, int flag, int argc,
+                        char *const argv[])
+{
+	struct udevice *blk, *parent;
+	struct udevice *dev;
+	struct uclass *uc;
+	int devnum, id;
+	char **params;
+	int ret, i = 0;
+
+	if (argc < 2)
+		return CMD_RET_USAGE;
+
+	params = malloc(argc * sizeof(char *));
+	if (IS_ERR_OR_NULL(params))
+		return CMD_RET_FAILURE;
+	for (i = 0; i < argc; i++)
+		params[i] = strdup(argv[i]);
+
+	switch (argc) {
+	case 2:
+		if (strncmp(argv[1], "list", 4) == 0) {
+			i = 0;
+			uclass_id_foreach_dev(UCLASS_BLK, dev, uc)
+				block_display_line(dev, i++);
+
+			return CMD_RET_SUCCESS;
+		}
+		break;
+	case 3:
+		ret = CMD_RET_USAGE;
+		if (strncmp(argv[1], "inf", 3) == 0)
+			devnum = (int)dectoul(argv[2], NULL);
+		else if (strncmp(argv[1], "dev", 3) == 0)
+			devnum = (int)dectoul(argv[2], NULL);
+		else if (strncmp(argv[1], "part", 4) == 0)
+			devnum = (int)dectoul(argv[2], NULL);
+		else
+			goto failed;
+
+		if (!uclass_get_device(UCLASS_BLK, devnum, &blk)) {
+			parent = dev_get_parent(blk);
+			if (IS_ERR_OR_NULL(parent)) {
+				ret = CMD_RET_FAILURE;
+				goto failed;
+			}
+			id = device_get_uclass_id(parent);
+			switch (id) {
+			case UCLASS_MMC:
+				if_type = IF_TYPE_MMC;
+				break;
+			case UCLASS_MTD:
+				if_type = IF_TYPE_MTD;
+				break;
+			default:
+				debug("Not supported uclass_id (%d)\n", id);
+				ret = CMD_RET_FAILURE;
+				goto failed;
+			}
+			blk_dev = devnum;
+			if_devnum = dev_seq(parent);
+
+			if (strncmp(argv[1], "inf", 3) == 0) {
+				free(params[1]);
+				params[1] = strdup("dev");
+			}
+			params[2] = strdup(simple_itoa(if_devnum));
+			printf("%s device %d is now current block\n",
+			            blk_get_if_type_name(if_type), if_devnum);
+		} else {
+			ret = CMD_RET_FAILURE;
+			goto failed;
+		}
+	}
+
+	if (blk_dev < 0 || !if_type) {
+		printf("\n ** Invaild device number or interface type **\n\n");
+		ret = CMD_RET_USAGE;
+		goto failed;
+	}
+
+	switch ((int)if_type) {
+	case IF_TYPE_MMC:
+		ret = do_mmc_block(cmdtp, flag, argc, params);
+		break;
+	case IF_TYPE_MTD:
+		ret = do_mtdblock(cmdtp, flag, argc, params);
+		break;
+	default:
+		ret = CMD_RET_FAILURE;
+		debug("Not supported if_type (%d)", if_type);
+	}
+
+failed:
+	for (i = 0; i < argc; i++)
+		free(params[i]);
+	free(params);
+	return ret;
+}
+
+U_BOOT_CMD(
+	block, 8, 1, do_block,
+	"block devices sub-system",
+	"list - list all available block devices\n"
+	"block info [dev] - show one or all devices of a specified if_type\n"
+	"block dev [dev] - show or set current device of a specified if_type\n"
+	"block part [dev] - print partition table for one or all devices of a specified if_type\n"
+	"block read addr blk# cnt - read `cnt' blocks starting at block\n"
+	"     `blk#' to memory address `addr'\n"
+	"block write addr blk# cnt - write `cnt' blocks starting at block\n"
+	"     `blk#' from memory address `addr'"
+);
