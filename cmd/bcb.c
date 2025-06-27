2a1204b4ff0e64416ac5fa8e940ced9d4b1bfde8
chasinglulu <wangkartx@gmail.com>
2025-06-27 11:00:27 +0800

    cmd: bcb: rewrite load and store

--- cmd/bcb.c
+++ cmd/bcb.c
@@ -32,7 +32,10 @@ static int bcb_dev                             = -1;
 static int bcb_part                            = -1;
 static struct blk_desc *bcb_dev_desc           = NULL;
 static struct disk_partition bcb_part_info     = { 0 };
-static struct bootloader_message bcb __aligned(ARCH_DMA_MINALIGN) = { { 0 } };
+static struct bootloader_message bcb           = { 0 };
+static struct bootloader_message_ab bl_msg_ab  = { 0 };
+bool bcb_loaded = false;
+bool bcb_need_stored = false;
 
 static int bcb_cmd_get(char *cmd)
 {
@@ -58,6 +61,10 @@ static int bcb_is_misused(int argc, char *const argv[])
 
 	switch (cmd) {
 	case BCB_CMD_LOAD:
+	case BCB_CMD_STORE:
+		if (argc != 1 && argc != 3)
+			goto err;
+		break;
 	case BCB_CMD_FIELD_SET:
 		if (argc != 3)
 			goto err;
@@ -67,13 +74,6 @@ static int bcb_is_misused(int argc, char *const argv[])
 			goto err;
 		break;
 	case BCB_CMD_FIELD_CLEAR:
-		if (argc != 1 && argc != 2)
-			goto err;
-		break;
-	case BCB_CMD_STORE:
-		if (argc != 1 && argc != 3)
-			goto err;
-		break;
 	case BCB_CMD_FIELD_DUMP:
 		if (argc != 1 && argc != 2)
 			goto err;
@@ -115,10 +115,13 @@ static int bcb_field_get(char *name, char **fieldp, int *sizep)
 	return 0;
 }
 
-static int bcb_load(struct blk_desc *dev_desc, struct disk_partition *part_info)
+static int bcb_create_from_disk(struct blk_desc *dev_desc, struct disk_partition *part_info,
+                              struct bootloader_message **bcbp)
 {
 	ulong blk_offset, blkcnt, offset;
-	char buf[sizeof(struct bootloader_message_ab)];
+	struct bootloader_message *bcb;
+	char *buf;
+	size_t size;
 	int ret;
 
 	offset = offsetof(struct bootloader_message_ab, message);
@@ -130,17 +133,31 @@ static int bcb_load(struct blk_desc *dev_desc, struct disk_partition *part_info)
 	blkcnt = DIV_ROUND_UP(sizeof(struct bootloader_message), part_info->blksz);
 	if (unlikely(blkcnt > part_info->size)) {
 		pr_err("Partition size too small\n");
-		ret = -EINVAL;
-		goto err;
+		return -EINVAL;
+	}
+
+	size = roundup(sizeof(struct bootloader_message_ab), part_info->blksz);
+	buf = malloc_cache_aligned(size);
+	if (!buf) {
+		pr_err("Out of memory\n");
+		return -ENOMEM;
 	}
 
 	ret = blk_dread(dev_desc, part_info->start + blk_offset,
 					blkcnt, buf);
 	if (ret != blkcnt) {
-		pr_err("Unable to read BCB from part '%s' on '%s' device.\n",
+		pr_err("Unable to read BCB from partition '%s' on '%s' device.\n",
 		                 part_info->name, dev_desc->bdev->name);
-		ret = -EIO;
-		goto err;
+		free(buf);
+		return -EIO;
+	}
+
+	size = roundup(sizeof(struct bootloader_message), part_info->blksz);
+	bcb = malloc_cache_aligned(size);
+	if (!bcb) {
+		pr_err("Could not allocate BCB memory\n");
+		free(buf);
+		return -ENOMEM;
 	}
 
 	if (blk_offset == 0)
@@ -148,18 +165,105 @@ static int bcb_load(struct blk_desc *dev_desc, struct disk_partition *part_info)
 	else
 		memcpy(&bcb, buf, sizeof(struct bootloader_message));
 
-	bcb_dev = dev_desc->devnum;
-	printf("BCB loaded from part '%s' on '%s' device successfully.\n",
+	debug("BCB loaded from part '%s' on '%s' device successfully.\n",
 	                    part_info->name, dev_desc->bdev->name);
 
+	*bcbp = bcb;
+	free(buf);
 	return 0;
+}
 
-err:
-	bcb_dev = -1;
-	bcb_part = -1;
-	bcb_dev_desc = NULL;
-	memset(&bcb_part_info, 0, sizeof(bcb_part_info));
-	return ret;
+static int bcb_create_from_buffer(struct bootloader_message_ab *buffer,
+							  struct bootloader_message **bcbp)
+{
+	struct bootloader_message *bcb;
+	loff_t offset;
+	int size;
+
+	if (!buffer || !bcbp) {
+		pr_err("Invalid arguments\n");
+		return -EINVAL;
+	}
+
+	size = roundup(sizeof(struct bootloader_message), sizeof(*buffer));
+	bcb = malloc_cache_aligned(size);
+	if (!bcb) {
+		pr_err("Out of memory\n");
+		return -ENOMEM;
+	}
+
+	offset = offsetof(struct bootloader_message_ab, message);
+	memcpy(bcb, (void *)buffer + offset, sizeof(struct bootloader_message));
+	*bcbp = bcb;
+
+	debug("BCB loaded from buffer successfully.\n");
+	return 0;
+}
+
+static int bcb_load_from_disk(const char *ifname, const char *dev_part_str)
+{
+	struct bootloader_message *bcb_metadata = NULL;
+	struct blk_desc *dev_desc = NULL;
+	struct disk_partition part_info = { 0 };
+	int part = -1;
+	int ret;
+
+	/* Lookup the "misc" partition */
+	ret = part_get_info_by_dev_and_name_or_num(ifname, dev_part_str,
+	      &dev_desc, &part_info,
+	      false);
+	if (ret < 0) {
+		pr_err("Unable to get device and partition information\n");
+		return ret;
+	}
+	part = ret;
+
+	ret = bcb_create_from_disk(dev_desc, &part_info, &bcb_metadata);
+	if (ret < 0) {
+		pr_err("Could not load BCB from '%s %s'\n",
+		            ifname, dev_part_str);
+		return ret;
+	}
+
+	memcpy(&bcb, bcb_metadata, sizeof(bcb));
+	bcb_loaded = true;
+	free(bcb_metadata);
+
+	bcb_dev_desc = dev_desc;
+	bcb_part_info = part_info;
+	bcb_dev = dev_desc->devnum;
+	bcb_part = part;
+
+	printf("BCB loaded from '%s %s' successfully.\n",
+	                             ifname, dev_part_str);
+	return 0;
+}
+
+static int bcb_load_from_buffer(void)
+{
+	ALLOC_CACHE_ALIGN_BUFFER(struct bootloader_message_ab, buffer, 1);
+	struct bootloader_message *bcb_metadata = NULL;
+	int ret;
+
+	ret = bootloader_message_ab_load(buffer);
+	if (ret < 0) {
+		pr_err("Could not load BCB from buffer.\n");
+		return ret;
+	}
+
+	ret = bcb_create_from_buffer(buffer, &bcb_metadata);
+	if (ret < 0) {
+		pr_err("Could not create BCB from buffer.\n");
+		return ret;
+	}
+
+	memcpy(&bl_msg_ab, buffer, sizeof(bl_msg_ab));
+	memcpy(&bcb, bcb_metadata, sizeof(bcb));
+	bcb_loaded = true;
+	free(bcb_metadata);
+
+	printf("BCB loaded from buffer successfully.\n");
+	return 0;
 }
 
 static int do_bcb_load(struct cmd_tbl *cmdtp, int flag, int argc,
@@ -167,28 +271,24 @@ static int do_bcb_load(struct cmd_tbl *cmdtp, int flag, int argc,
 {
 	int ret;
 
-	if (argc != 2) {
+	if (argc != 0 && argc != 2) {
 		pr_err("Invalid number of arguments\n");
 		return CMD_RET_USAGE;
 	}
 
-	/* Lookup the "misc" partition */
-	ret = part_get_info_by_dev_and_name_or_num(argv[0], argv[1],
-	      &bcb_dev_desc, &bcb_part_info,
-	      false);
-	if (ret < 0) {
-		pr_err("Unable to get device and partition information\n");
-		return CMD_RET_FAILURE;
+	switch (argc) {
+	case 0:
+		ret = bcb_load_from_buffer();
+		break;
+	case 2:
+		ret = bcb_load_from_disk(argv[0], argv[1]);
+		break;
 	}
 
-	ret = bcb_load(bcb_dev_desc, &bcb_part_info);
 	if (ret < 0) {
-		pr_err("Could not load BCB part '%s' on '%s' device\n",
-		            bcb_part_info.name, bcb_dev_desc->bdev->name);
+		pr_err("Failed to load BCB\n");
 		return CMD_RET_FAILURE;
 	}
-	bcb_dev = bcb_dev_desc->devnum;
-	bcb_part = ret;
 
 	return CMD_RET_SUCCESS;
 }
@@ -222,13 +322,14 @@ static int __bcb_set(char *fieldp, const char *valp)
 	}
 	free(str);
 
+	bcb_need_stored = true;
 	return CMD_RET_SUCCESS;
 }
 
 static int do_bcb_set(struct cmd_tbl *cmdtp, int flag, int argc,
                       char * const argv[])
 {
-	if (bcb_dev == -1 || bcb_part == -1) {
+	if (!bcb_loaded) {
 		pr_err("\n ** No loaded BCB metadata **\n\n");
 		return CMD_RET_USAGE;
 	}
@@ -242,13 +343,14 @@ static int do_bcb_clear(struct cmd_tbl *cmdtp, int flag, int argc,
 	int size;
 	char *field;
 
-	if (bcb_dev == -1 || bcb_part == -1) {
+	if (!bcb_loaded) {
 		pr_err("\n ** No loaded BCB metadata **\n\n");
 		return CMD_RET_USAGE;
 	}
 
 	if (argc == 0) {
 		memset(&bcb, 0, sizeof(bcb));
+		bcb_need_stored = true;
 		return CMD_RET_SUCCESS;
 	}
 
@@ -256,6 +358,7 @@ static int do_bcb_clear(struct cmd_tbl *cmdtp, int flag, int argc,
 		return CMD_RET_FAILURE;
 
 	memset(field, 0, size);
+	bcb_need_stored = true;
 
 	return CMD_RET_SUCCESS;
 }
@@ -267,7 +370,7 @@ static int do_bcb_test(struct cmd_tbl *cmdtp, int flag, int argc,
 	char *field;
 	char *op = argv[1];
 
-	if (bcb_dev == -1 || bcb_part == -1) {
+	if (!bcb_loaded) {
 		pr_err("\n ** No loaded BCB metadata **\n\n");
 		return CMD_RET_USAGE;
 	}
@@ -298,7 +401,7 @@ static int do_bcb_dump(struct cmd_tbl *cmdtp, int flag, int argc,
 	int size;
 	char *field;
 
-	if (bcb_dev == -1 || bcb_part == -1) {
+	if (!bcb_loaded) {
 		pr_err("\n ** No loaded BCB metadata **\n\n");
 		return CMD_RET_USAGE;
 	}
@@ -316,21 +419,14 @@ static int do_bcb_dump(struct cmd_tbl *cmdtp, int flag, int argc,
 	return CMD_RET_SUCCESS;
 }
 
-static int bcb_store(struct bootloader_message *bcb)
+static int bcb_write_into_disk(struct blk_desc *dev_desc,
+                               const struct disk_partition *part_info,
+                               struct bootloader_message *bcbp)
 {
 	ulong offset, blkcnt, blk_offset;
-	char buf[sizeof(struct bootloader_message_ab)];
+	size_t size;
+	char *buf;
 	int ret;
-
-	if (unlikely(!bcb)) {
-		pr_err("Invalid BCB metadata\n");
-		return -EINVAL;
-	}
-
-	if (bcb_dev < 0 || bcb_part < 0) {
-		pr_err("Invalid device or partition\n");
-		return -EINVAL;
-	}
 
 	offset = offsetof(struct bootloader_message_ab, message);
 	if (offset >= bcb_part_info.blksz)
@@ -339,25 +435,132 @@ static int bcb_store(struct bootloader_message *bcb)
 		blk_offset = 0;
 	blkcnt = DIV_ROUND_UP(sizeof(struct bootloader_message), bcb_part_info.blksz);
 
+	size = roundup(sizeof(struct bootloader_message_ab), part_info->blksz);
+	buf = malloc_cache_aligned(size);
+	if (!buf) {
+		pr_err("Out of memory\n");
+		return -ENOMEM;
+	}
+
 	ret = blk_dread(bcb_dev_desc, bcb_part_info.start + blk_offset,
 	                            blkcnt, buf);
 	if (IS_ERR_VALUE(ret)) {
 		pr_err("Could not read from the '%s' partition\n", bcb_part_info.name);
+		free(buf);
 		return -EIO;
 	}
 
 	if (blk_offset == 0)
-		memcpy(buf + offset, bcb, sizeof(struct bootloader_message));
+		memcpy(buf + offset, bcbp, sizeof(struct bootloader_message));
 	else
-		memcpy(buf, bcb, sizeof(struct bootloader_message));
+		memcpy(buf, bcbp, sizeof(struct bootloader_message));
 
 	ret = blk_dwrite(bcb_dev_desc, bcb_part_info.start + blk_offset,
 	                    blkcnt, buf);
 	if (IS_ERR_VALUE(ret)) {
-		pr_err("Could not write back the '%s' partition\n", bcb_part_info.name);
+		pr_err("Could not write into the '%s' partition\n", bcb_part_info.name);
+		free(buf);
 		return -EIO;
 	}
 
+	free(buf);
+	return 0;
+}
+
+static int bcb_write_into_buffer(struct bootloader_message_ab *buffer,
+							  struct bootloader_message *bcbp)
+{
+	ulong offset;
+
+	if (!buffer || !bcbp) {
+		pr_err("Invalid arguments\n");
+		return -EINVAL;
+	}
+
+	offset = offsetof(struct bootloader_message_ab, message);
+	if (offset >= sizeof(*buffer)) {
+		pr_err("Offset is out of bounds\n");
+		return -EINVAL;
+	}
+
+	memcpy((void *)buffer + offset, bcbp, sizeof(struct bootloader_message));
+	return 0;
+}
+
+static int bcb_store_with_dev_part_str(const char *ifname,
+									const char *dev_part_str,
+									struct bootloader_message *bcbp)
+{
+	struct blk_desc *dev_desc = NULL;
+	struct disk_partition part_info = { 0 };
+	int ret;
+
+	ret = part_get_info_by_dev_and_name_or_num(ifname, dev_part_str,
+	      &dev_desc, &part_info,
+	      false);
+	if (ret < 0) {
+		pr_err("Unable to get partition information\n");
+		return ret;
+	}
+
+	ret = bcb_write_into_disk(dev_desc, &part_info, bcbp);
+	if (ret < 0) {
+		pr_err("Failed to store BCB into '%s %s'\n",
+		               ifname, dev_part_str);
+		return ret;
+	}
+
+	printf("BCB stored into '%s %s' successfully.\n",
+	                ifname, dev_part_str);
+	return 0;
+}
+
+static int bcb_store_into_disk(struct blk_desc *dev_desc,
+                               const struct disk_partition *part_info,
+                               struct bootloader_message *bcbp)
+{
+	int ret;
+
+	if (!dev_desc || !part_info || !bcbp) {
+		pr_err("Invalid arguments\n");
+		return -EINVAL;
+	}
+
+	ret = bcb_write_into_disk(dev_desc, part_info, bcbp);
+	if (ret < 0) {
+		pr_err("Could not store BCB into partition '%s' on '%s' device\n",
+		                  part_info->name, dev_desc->bdev->name);
+		return ret;
+	}
+
+	printf("BCB stored into partition '%s' on '%s' device successfully.\n",
+	                part_info->name, dev_desc->bdev->name);
+	return 0;
+}
+
+static int bcb_store_into_buffer(struct bootloader_message_ab *buffer,
+							  struct bootloader_message *bcbp)
+{
+	int ret;
+
+	if (!buffer || !bcbp) {
+		pr_err("Invalid arguments\n");
+		return -EINVAL;
+	}
+
+	ret = bcb_write_into_buffer(buffer, bcbp);
+	if (ret < 0) {
+		pr_err("Unable to write BCB into buffer\n");
+		return ret;
+	}
+
+	ret = bootloader_message_ab_store(buffer);
+	if (ret < 0) {
+		pr_err("Could not store AB-specific bootloader message.\n");
+		return ret;
+	}
+
+	printf("BCB stored into buffer successfully.\n");
 	return 0;
 }
 
@@ -368,76 +571,76 @@ static int do_bcb_store(struct cmd_tbl *cmdtp, int flag, int argc,
 
 	if (argc != 0 && argc != 2) {
 		pr_err("Invalid number of arguments\n");
-		goto err;
+		return CMD_RET_USAGE;
 	}
 
-	if (argc ==0 && (bcb_dev < 0 || bcb_part < 0)) {
-		pr_err("Please load BCB first.\n");
+	if (!bcb_loaded) {
+		pr_err("No BCB loaded. Please load it first.\n");
 		return CMD_RET_FAILURE;
 	}
 
-	if (argc == 0)
-		goto store_bcb;
-
-	ret = part_get_info_by_dev_and_name_or_num(argv[0], argv[1],
-	      &bcb_dev_desc, &bcb_part_info,
-	      false);
-	if (ret < 0) {
-		pr_err("Unable to get partition information\n");
-		goto err;
-	}
-	bcb_part = ret;
-	bcb_dev = bcb_dev_desc->devnum;
-
-store_bcb:
-	ret = bcb_store(&bcb);
-	if (ret < 0) {
-		pr_err("Could not store BCB into part '%s' on '%s' device\n",
-		                  bcb_part_info.name, bcb_dev_desc->bdev->name);
-		goto err;
+	switch (argc) {
+	case 0:
+		if (bcb_part < 0 || bcb_dev < 0)
+			ret = bcb_store_into_buffer(&bl_msg_ab, &bcb);
+		else
+			ret = bcb_store_into_disk(bcb_dev_desc, &bcb_part_info,
+			                      &bcb);
+		break;
+	case 2:
+		ret = bcb_store_with_dev_part_str(argv[0], argv[1],
+		                       &bcb);
+		break;
 	}
 
-	printf("BCB stored into part '%s' on '%s' device successfully.\n",
-	                bcb_part_info.name, bcb_dev_desc->bdev->name);
+	if (ret < 0) {
+		pr_err("Failed to store BCB\n");
+		return CMD_RET_FAILURE;
+	}
+
 	return CMD_RET_SUCCESS;
-
-err:
-	bcb_dev_desc = NULL;
-	memset(&bcb_part_info, 0, sizeof(bcb_part_info));
-	bcb_dev = -1;
-	bcb_part = -1;
-
-	return CMD_RET_FAILURE;
 }
 
 int bcb_write_reboot_reason(const char *ifname, int devnum,
                             const char *part_name, const char *reasonp)
 {
+	char dev_part_str[PART_NAME_LEN];
+	bool is_disk = false;
 	int ret;
-	char dev_part_str[64];
 
-	sprintf(dev_part_str, "%d#%s",devnum, part_name);
-	debug("%s: Writing reboot reason '%s' to BCB on '%s %s'\n",
-	            __func__, reasonp, ifname, dev_part_str);
-	ret = part_get_info_by_dev_and_name_or_num(ifname, dev_part_str,
-	      &bcb_dev_desc, &bcb_part_info,
-	      false);
-	if (ret < 0) {
-		pr_err("Unable to get device and partition information (ret = %d)\n", ret);
-		return CMD_RET_FAILURE;
+	if (!reasonp) {
+		pr_err("Invalid reboot reason\n");
+		return -EINVAL;
 	}
 
-	ret = bcb_load(bcb_dev_desc, &bcb_part_info);
-	if (ret != CMD_RET_SUCCESS)
+	is_disk = (ifname != NULL && part_name != NULL && devnum >= 0);
+
+	if (!is_disk) {
+		ret = bcb_load_from_buffer();
+	} else {
+		snprintf(dev_part_str, PART_NAME_LEN, "%d#%s", devnum, part_name);
+		ret = bcb_load_from_disk(ifname, dev_part_str);
+	}
+
+	if (ret < 0) {
+		pr_err("Failed to load BCB\n");
 		return ret;
+	}
 
 	ret = __bcb_set("command", reasonp);
 	if (ret != CMD_RET_SUCCESS)
 		return ret;
 
-	ret = bcb_store(&bcb);
-	if (ret != CMD_RET_SUCCESS)
+	if (!is_disk) {
+		ret = bcb_store_into_buffer(&bl_msg_ab, &bcb);
+	} else {
+		ret = bcb_store_with_dev_part_str(ifname, dev_part_str, &bcb);
+	}
+
+	if (ret < 0) {
+		pr_err("Failed to store BCB\n");
 		return ret;
+	}
 
 	return 0;
 }
@@ -454,6 +657,7 @@ static struct cmd_tbl cmd_bcb_sub[] = {
 static int do_bcb(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	struct cmd_tbl *c;
+	int ret;
 
 	if (argc < 2)
 		return CMD_RET_USAGE;
@@ -477,17 +681,40 @@ static int do_bcb(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 	argc--;
 	argv++;
 
-	return c->cmd(cmdtp, flag, argc, argv);
+	ret = c->cmd(cmdtp, flag, argc, argv);
+	if (ret < 0) {
+		pr_err("Command failed with error %d\n", ret);
+		return CMD_RET_FAILURE;
+	}
+
+	if (bcb_need_stored && bcb_loaded) {
+		/* Store the BCB if it has been modified */
+		if (bcb_part < 0 || bcb_dev < 0) {
+			ret = bcb_store_into_buffer(&bl_msg_ab, &bcb);
+		} else {
+			ret = bcb_store_into_disk(bcb_dev_desc, &bcb_part_info,
+			                      &bcb);
+		}
+		if (ret < 0) {
+			pr_err("Unable to store BCB\n");
+			return CMD_RET_FAILURE;
+		}
+		bcb_need_stored = false;
+	}
+
+	return CMD_RET_SUCCESS;
 }
 
 U_BOOT_CMD(
 	bcb, CONFIG_SYS_MAXARGS, 1, do_bcb,
 	"Load/store/set/clear/test/dump Android BCB fields",
-	"load <interface> <dev[:part|#part_name]>\n"
+	"load [<interface>] [<dev[:part|#part_name]>]\n"
 	"    - load BCB from 'part' on device type 'interface' instance 'dev'\n"
+	"      If not specified, the default interface, dev and part is used.\n"
 	"bcb store [<interface>] [<dev[:part|#part_name]>]\n"
 	"    - store BCB to 'part' on device type 'interface' instance 'dev';\n"
-	"      if not specified, reuses the location from 'bcb load'\n"
+	"      if not specified, reuses the location from 'abc load' or use the default\n"
+	"      location\n"
 	"bcb set <field> <val>\n"
 	"    - set BCB <field> to <val>\n"
 	"bcb clear [<field>]\n"
